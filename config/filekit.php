bd9d2e8c96ddafe51804bf83d5fd207221a0e50d
Jomon Johnson <jomon.entero@gmail.com>
2019-02-26 22:13:57 +0530

    init

---
+++ config/filekit.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jomon
+ * Date: 26/2/19
+ * Time: 9:31 PM
+ */
+
+return [
+    'controllerMap' => [
+        'file-storage' => [
+            'class' => \codexten\yii\filekit\controllers\FileStorageController::class,
+        ],
+    ],
+];
