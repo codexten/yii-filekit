572bde400062e332b6b5475114ca5464df7f3137
Jomon Johnson <jomon.entero@gmail.com>
2019-05-08 12:34:13 +0530

    init

--- config/filekit.php
+++ config/filekit.php
@@ -6,10 +6,21 @@
  * Time: 9:31 PM
  */
 
+use codexten\yii\filekit\controllers\FileStorageController;
+use trntv\filekit\widget\Upload;
+
 return [
     'controllerMap' => [
         'file-storage' => [
-            'class' => \codexten\yii\filekit\controllers\FileStorageController::class,
+            'class' => FileStorageController::class,
+        ],
+    ],
+    'container' => [
+        'definitions' => [
+            Upload::class => [
+                'url' => ['/file-storage/upload'],
+                'maxFileSize' => 5000000, // 5 MiB
+            ],
         ],
     ],
 ];
